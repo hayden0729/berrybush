79475253307b9a3a1b5723936b10b225f14bf19d
hayden0729 <haydenmcreate@gmail.com>
2024-05-20 14:10:09 -0400

    Increment version number to 1.4.2

--- berrybush/__init__.py
+++ berrybush/__init__.py
@@ -46,7 +46,7 @@ ICONS: bpy.utils.previews.ImagePreviewCollection
 bl_info = {
     "name" : "BRRES format (BerryBush)",
     "author": "hayden0729",
-    "version": (1, 4, 1),
+    "version": (1, 4, 2),
     "blender" : (3, 3, 0),
     "location": "File > Import-Export",
     "description": "NSMBW focused BRRES support",
